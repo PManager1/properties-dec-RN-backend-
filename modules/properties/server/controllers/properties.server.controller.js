64c22c86fd43afbb1321d43dc9c1b6530443a24f
jpca999 <jpca999@gmail.com>
2016-12-08 21:29:18 -0500

    WORKING BASED ON WHO LOGGED IN 2

--- modules/properties/server/controllers/properties.server.controller.js
+++ modules/properties/server/controllers/properties.server.controller.js
@@ -95,7 +95,7 @@ exports.list = function(req, res) {
 
 
 exports.propertiesListByUser = function(req, res, next, id) {
-  Property.find({user_logged_in: 'jp'}).sort('-created').populate('user', 'displayName').exec(function(err, properties) {
+  Property.find({user_logged_in: id }).sort('-created').populate('user', 'displayName').exec(function(err, properties) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
