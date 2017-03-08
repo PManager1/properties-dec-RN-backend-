8c02b569ee845e376eec62acf2ba6e1ac3ecac82
jpca999 <jpca999@gmail.com>
2017-03-07 20:26:55 -0500

    -

--- modules/properties/server/controllers/properties.server.controller.js
+++ modules/properties/server/controllers/properties.server.controller.js
@@ -6,6 +6,7 @@
 var path = require('path'),
   mongoose = require('mongoose'),
   Property = mongoose.model('Property'),
+  // newReifax = mongoose.model('newReifax'),  
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
   _ = require('lodash'),
   config = require(path.resolve('./config/config')),
